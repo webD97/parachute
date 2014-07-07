bd284d0a0a74954ee0ca4523c4439b13841a11a1
webdesigner97 <Christian_D_97@gmx.de>
2014-07-07 10:34:41 +0200

    lots of stuff

--- init.lua
+++ init.lua
@@ -12,10 +12,15 @@ minetest.register_craftitem("parachute:parachute_item", {
 			-- Spawn parachute
 			pos.y = pos.y + 3
 			local ent = minetest.add_entity(pos, "parachute:parachute")
+			-- Spawn speed watcher
+			pos.y = pos.y + 1
+			local watcher = minetest.add_entity(pos, "parachute:watcher")
+			-- Attach speed watcher
+			watcher:set_attach(user, "", {x=0,y=0,z=0}, {x=0,y=0,z=0})
+			-- Copy watcher velocity to parachute
+			ent:setvelocity({x = 0, y = watcher:getvelocity().y, z = 0})
 			-- Attach user
 			user:set_attach(ent, "", {x=0,y=0,z=0}, {x=0,y=0,z=0})
-			-- Copy user velocity to parachute
-			ent.object:setvelocity({x = 0, y = user:getvelocity().y, z = 0})
 			-- Tell it who is attached
 			ent = ent:get_luaentity()
 			ent.attached = user:get_player_name()
@@ -34,6 +39,12 @@ local parachute = {
 	},
 	attached = nil
 }
+local watcher = {
+	initial_properties = {
+		visual = "wielditem",
+		collisionbox = {0,0,0,0,0,0},
+	}
+}
 
 function parachute:on_step(dtime)
 	if self.attached ~= nil then
@@ -45,7 +56,11 @@ function parachute:on_step(dtime)
 		self.object:set_detach()
 		self.object:remove()
 	end
+end
+
+function watcher:on_step(dtime)
 	minetest.chat_send_all(minetest.serialize(self.object:getvelocity()))
 end
 
 minetest.register_entity("parachute:parachute", parachute)
+minetest.register_entity("parachute:watcher", watcher)
